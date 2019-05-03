aa19874b2cc03205dfc10dca298ed84e280bb07a
김 태헌 <xxssagaji@gmail.com>
2019-05-03 23:17:39 +0900

    API 구했음

--- 4USD_CUR/MainWindow.xaml.cs
+++ 4USD_CUR/MainWindow.xaml.cs
@@ -13,16 +13,45 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace _4USD_CUR
 {
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
+    /// 
+   
     public partial class MainWindow : Window
     {
+        string sReqStr = "";
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 버튼 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string sUrl = "http://api.manana.kr/exchange/rate/";
+            sUrl += "USD/"+ cboCurrency.SelectionBoxItem.ToString() + ".Json";
+
+
+        }
+
+        private void BtnNotice_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("이 프로그램은 4달러를 여러 통화단위로 변환해주는 프로그램 입니다. \n환율정보는 오차가 있을 수 있습니다. \nPowered By pureani.tistory.com", "알림");
+        }
+
+
     }
 }
